f72a61e79efe22bb48b7b594eb2a6980a5968d11
bala-amypo <bala@amypo.in>
2025-12-17 05:04:42 +0000

    User commit

--- src/main/java/com/example/demo/model/ClaimRule.java
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -1,5 +1,7 @@
+import.jakarta.persistence.id;
+import.jakarta.persistence.Entity;
 public class ClaimRule
-{
+{   @Id
     private Long id;
     private String ruleName;
     private String conditionExpression;

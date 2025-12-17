bea88f0b4132b6b4357b8d323ff37cc6ba17d87e
bala-amypo <bala@amypo.in>
2025-12-17 05:34:41 +0000

    User commit

--- src/main/java/com/example/demo/service/ClaimRuleService.java
+++ src/main/java/com/example/demo/service/ClaimRuleService.java
@@ -1,10 +1,10 @@
 import org.springframework.stereotype.Service;
 import.java.util.Map;
 import.java.util.Hashmap;
-import.com.example.demo.model
+import.com.example.demo.model.ClaimRule;   
 
 @service
 public class ClaimRuleService
 {
-
+  Map<Float,ClaimRule>
 }

eba8d541782fa68e5cbb250533d7b496632114fe
bala-amypo <bala@amypo.in>
2025-12-18 06:15:37 +0000

    User commit

--- src/main/java/com/example/demo/service/imp/ClaimRuleServiceimp.java
+++ src/main/java/com/example/demo/service/imp/ClaimRuleServiceimp.java
@@ -1,9 +1,17 @@
 import org.springframework.stereotype.Service;
-import org.springframework.beans
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.ClaimRule;
+import com.example.demo.repository.ClaimRuleRepository;
+import com.example.demo.service.ClaimRuleService;
+
 @Service
 public class ClaimRuleServiceimp implements ClaimRuleService{
     @Autowired
     ClaimRuleRepository crr;
+    public ClaimRule addRule(ClaimRule rule)
+    {
+        crr.
+    }
 
     
 }

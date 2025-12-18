92bc4ea7bddc2666df0f64bde1215536bc45b818
bala-amypo <bala@amypo.in>
2025-12-18 05:20:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRuleRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRuleRepository.java
@@ -1,6 +1,7 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.ClaimRule;
-public interface ClaimRuleRepository extends JpaRepository<ClaimRule,Long>
-{
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.ClaimRule;
+// public interface ClaimRuleRepository extends JpaRepository<ClaimRule,Long>
+// {
 
-}
+// }
+// 

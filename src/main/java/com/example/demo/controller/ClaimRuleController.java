475027ca2d76cab1ead9daa6d7b996dd80f66601
bala-amypo <bala@amypo.in>
2025-12-18 10:39:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/ClaimRuleController.java
+++ src/main/java/com/example/demo/controller/ClaimRuleController.java
@@ -3,9 +3,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.model.ClaimRule;
 import com.example.demo.service.ClaimRuleService;
+import java.util.List;
 
 @RestController
 public class ClaimRuleController{
@@ -14,12 +16,12 @@ public class ClaimRuleController{
     @PostMapping("/rules")
     public ClaimRule rules(@RequestBody ClaimRule rule)
     {
-        return crs.addrule(rule);
+        return crs.addRule(rule);
     }
     @GetMapping
     public List<ClaimRule> getrules()
     {
-        return crs.getAllrules()
+        return crs.getAllRules();
     }
 
 }

7bd5b0a37053824268cccd58eb5c367387925280
bala-amypo <bala@amypo.in>
2025-12-20 17:54:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Evidence.java
+++ src/main/java/com/example/demo/model/Evidence.java
@@ -1,13 +1,13 @@
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
-// @Entity
-// public class Evidence
-// {
-//   @Id
-//   @GeneratedValue(strategy=GenerationType.IDENTITY)
-//   private Long id;
+@Entity
+public class Evidence
+{
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Long id;
   
-// }
+}

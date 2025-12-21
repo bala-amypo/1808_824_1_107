6702541194d0cefeafc898fb1c8d6ace7f3fc601
bala-amypo <bala@amypo.in>
2025-12-21 09:46:48 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEngineUtil.java
+++ src/main/java/com/example/demo/util/RuleEngineUtil.java
@@ -1,4 +1,18 @@
-public class RuleEngineUtil
-{
-    
+package com.example.demo.util;
+
+import com.example.demo.model.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RuleEngineUtil {
+
+    public static Set<ClaimRule> evaluateRules(DamageClaim claim) {
+        // Dummy logic
+        return new HashSet<>();
+    }
+
+    public static Double calculateScore(Set<ClaimRule> rules) {
+        return rules.size() * 10.0;
+    }
 }

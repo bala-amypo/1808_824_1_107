74f4449035ab7be9de15051ff376dc974a64b3e9
bala-amypo <bala@amypo.in>
2025-12-21 05:43:50 +0000

    User commit

--- src/main/java/com/example/demo/model/DamageClaim.java
+++ src/main/java/com/example/demo/model/DamageClaim.java
@@ -1,27 +1,27 @@
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GeneratedType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
 
-// public class DamageClaim
-// {
-//     @Id
-//     @GenerateValue(strategy=GeneratedType.Identity)
-//     private Long id;
-//     private String claimDescription;
-//     private String status;
-//     private Double score;
+public class DamageClaim
+{
+    @Id
+    @GenerateValue(strategy=GeneratedType.Identity)
+    private Long id;
+    private String claimDescription;
+    private String status;
+    private Double score;
 
-// public Long getId()
-// {
-//     return id;
-// }
-// public void setid(Long id)
-// {
-//     this.id=id;
-// }
-// public String getClaimDes()
-// {
-//     return claimDes;
-// }
-// }
+public Long getId()
+{
+    return id;
+}
+public void setid(Long id)
+{
+    this.id=id;
+}
+public String getClaimDes()
+{
+    return claimDes;
+}
+}

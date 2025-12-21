835ae64761847b9bb492e6e19b7da4692a0ee7ed
bala-amypo <bala@amypo.in>
2025-12-21 06:01:49 +0000

    User commit

--- src/main/java/com/example/demo/model/DamageClaim.java
+++ src/main/java/com/example/demo/model/DamageClaim.java
@@ -1,30 +1,30 @@
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GeneratedType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GeneratedType;
+// import com.example.demo.model.parcel;
+// public class DamageClaim
+// {
+//     @Id
+//     @GenerateValue(strategy=GeneratedType.Identity)
+//     private Long id;
+//     @ManyToOne
+//     @joinColumn(name="parcel_id")
+//     private Parcel parcel;
+//     private String claimDescription;
+//     private String status;
+//     private Double score;
 
-public class DamageClaim
-{
-    @Id
-    @GenerateValue(strategy=GeneratedType.Identity)
-    private Long id;
-    @ManyToOne
-    @joinColumn(name="parcel_id")
-    private
-    private String claimDescription;
-    private String status;
-    private Double score;
-
-public Long getId()
-{
-    return id;
-}
-public void setid(Long id)
-{
-    this.id=id;
-}
-public String getClaimDes()
-{
-    return claimDes;
-}
-}
+// public Long getId()
+// {
+//     return id;
+// }
+// public void setid(Long id)
+// {
+//     this.id=id;
+// }
+// public String getClaimDes()
+// {
+//     return claimDes;
+// }
+// }

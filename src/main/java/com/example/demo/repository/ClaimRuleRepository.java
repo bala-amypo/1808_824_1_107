61d3ee5b74c3cb8c7468bd147bba2b014dddd0c7
bala-amypo <bala@amypo.in>
2025-12-25 12:00:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRuleRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRuleRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.ClaimRule;
-public interface ClaimRuleRepository extends JpaRepository<ClaimRule,Long>
-{
- 
+import java.util.List;
+
+public interface ClaimRuleRepository extends JpaRepository<ClaimRule, Long> {
+    List<ClaimRule> findAll();
 }
--- src/main/java/com/example/demo/repository/EvidenceRepository.java
+++ src/main/java/com/example/demo/repository/EvidenceRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Evidence;
-import com.example.demo.model.DamageClaim;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.model.Evidence;
 import java.util.List;
 
 public interface EvidenceRepository extends JpaRepository<Evidence, Long> {
-
-    List<Evidence> findByClaim(DamageClaim claim);
+    long countByClaim_Id(Long claimId);
+    List<Evidence> findByClaim_Id(Long claimId);
 }

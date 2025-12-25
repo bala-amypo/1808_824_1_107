a6cf6d08273c4d10d1aac732c6a75cd0a194fdf7
bala-amypo <bala@amypo.in>
2025-12-25 12:18:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/ClaimRuleController.java
+++ src/main/java/com/example/demo/controller/ClaimRuleController.java
@@ -1,27 +1,28 @@
 package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.model.ClaimRule;
 import com.example.demo.service.ClaimRuleService;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-public class ClaimRuleController{
-    @Autowired
-    ClaimRuleService crs;
-    @PostMapping("/rules")
-    public ClaimRule rules(@RequestBody ClaimRule rule)
-    {
-        return crs.addRule(rule);
-    }
-    @GetMapping
-    public List<ClaimRule> getrules()
-    {
-        return crs.getAllRules();
+@RequestMapping("/rules")
+public class ClaimRuleController {
+
+    private final ClaimRuleService ruleService;
+
+    public ClaimRuleController(ClaimRuleService ruleService) {
+        this.ruleService = ruleService;
     }
 
+    @PostMapping
+    public ClaimRule addRule(@RequestBody ClaimRule rule) {
+        return ruleService.addRule(rule);
+    }
+
+    @GetMapping
+    public List<ClaimRule> getAllRules() {
+        return ruleService.getAllRules();
+    }
 }

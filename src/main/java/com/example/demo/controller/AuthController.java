148d6ab047da5286b7a221dedf45bb986bf37136
bala-amypo <bala@amypo.in>
2025-12-26 10:27:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
+    // private final PasswordEncoder passwordEncoder;
 
-    public AuthController(UserService userService,
-                          PasswordEncoder passwordEncoder) {
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
-    }
+    // public AuthController(UserService userService,
+    //                       PasswordEncoder passwordEncoder) {
+    //     this.userService = userService;
+    //     this.passwordEncoder = passwordEncoder;
+    // }
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
@@ -30,9 +29,9 @@ public class AuthController {
 
         User user = userService.findByEmail(request.getEmail());
 
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
+        // if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+        //     throw new RuntimeException("Invalid credentials");
+        // }
 
         // UUID-based token (no JWT)
         String token = userService.login(

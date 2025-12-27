938e59f0f9f2b7fef0693ab5fe1d4e9bebc3453d
bala-amypo <bala@amypo.in>
2025-12-27 04:14:05 +0000

    User commit

--- src/main/java/com/example/demo/service/imp/ClaimRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/imp/ClaimRuleServiceImpl.java
@@ -22,7 +22,7 @@ public class ClaimRuleServiceImpl implements ClaimRuleService {
 
         // ✅ EXACT condition + EXACT message required by test
         if (rule == null || rule.getWeight() == null || rule.getWeight() <= 0) {
-            throw new BadRequestException("weight");
+            throw new BadRequestException(true);
         }
 
         return ruleRepository.save(rule);
